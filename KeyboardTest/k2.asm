129557aa15b1205f5afbc9111dc668c0fb8f51e6
starkshaw <ttxzb@hotmail.com>
2015-03-18 23:47:40 +0000

    Modified Keyboard Test into: Press the SPACE will start the counter at the top right corner.

--- KeyboardTest/k2.asm
+++ KeyboardTest/k2.asm
@@ -1,9 +1,11 @@
 	.MODEL    small
 	.STACK
 	.DATA			; DS will may change when TSR operates
+	
 	.CODE
 
 	.STARTUP
+	dseg    db      '0','0','0','0' ; Store variables in code segment (TSR)
     push    es              ; Save existing value of ES.
 	mov 	ax,0000h    	; Set ES (extra segment) to page 0 
 	mov 	es,ax		; Note page 0 is vector address table
@@ -49,29 +51,66 @@ isr:	push	ax  		; Start of code you wish to call ever 50mS (18ms PC).
         mov dx,60h              ; Read the last scan code
         in  al,dx
         and  al,127             ; Mask MSB 1=Key Down, 0=Key Up
-        cmp  al,39h             ; Was a h/H pressed on the XT keyboard
+        cmp  al,39h             ; Was a space pressed on the XT keyboard
         jnz  ovr1
 
 	mov	ax,0b800h 	; Base address of screen memory
 	mov	es,ax
         mov     bx,140          ; Screen position (1,70)
-        mov     al,'1'          ; Print '1' on the screen
-        mov     es:[bx],al
-        inc		bx
-		mov     al,12
-		mov     es:[bx],al
-        jmp     ovr2
+        
+        mov     cx,4 
+        mov     bx,OFFSET dseg
+back:   mov     al,cs:[bx]      ; Load al with character code in [dseg]
+        inc     al		; Increase al by 1, next character.
+        mov     cs:[bx],al      ; Store the increased value
+        cmp     al,':'		; If al	equals character after 9
+        jnz     skip		; If it	is not 10 do nothing
+        mov     al,'0'		; Otherwise reset character to'0'
+        mov     cs:[bx],al      ;
+        inc     bx		; Move onto next digit
+        loop	back
+        
+skip:   mov     ax,0b800h       ; Base address of screen memory
+        mov     es,ax
+        mov     bx,OFFSET dseg  ; First digit
+        mov     dx,140		; Screen position (10,40)=2*(10*80+40)
+        mov     cx,4
+        
+nextc:  mov     al,cs:[bx]      ; Read character from our store  
+
+        mov     di,bx		; Exchange dx,bx
+        mov     bx,dx
+        mov     dx,di
+
+        mov     es:[bx],al      ; Character changed on screen to ASCII al
+
+        mov     di,bx		; Exchange dx,bx
+        mov     bx,dx
+        mov     dx,di
+
+        inc     bx
+        dec     dx
+        dec     dx  
+        loop    nextc
+        
+        
+        ;mov     al,'1'          ; Print '1' on the screen
+        ;mov     es:[bx],al
+        ;inc		bx
+		;mov     al,12
+		;mov     es:[bx],al
+        ;jmp     ovr2
 
 ovr1:   mov     ax,0b800h       ; If not an H overwrite top right with ' '
 		mov		es,ax
         mov     bx,140          ; Screen position (1,70)
         mov     cx,1
-again:  mov     al,'0'
-        mov     es:[bx],al
-        inc     bx
-        mov     al,12
-        mov     es:[bx],al
-        inc     bx
+again:  ;mov     al,'0'
+        ;mov     es:[bx],al
+        ;inc     bx
+        ;mov     al,12
+        ;mov     es:[bx],al
+        ;inc     bx
         loop    again
         
 
